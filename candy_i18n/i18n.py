c85cec8ab92919c11aeb871c0481072788a1cc62
faycheng <fei.cheng@daocloud.io>
2017-09-05 16:50:04 +0800

    rename argument: localedir -> locale_dir

--- candy_i18n/i18n.py
+++ candy_i18n/i18n.py
@@ -9,17 +9,17 @@ LOCALE_DIR = 'LOCALE_DIR'
 LANGUAGE = 'LANG'
 
 
-def translate(msg_id, msg_plural=None, plural_number=None, domain=None, localedir=None, lang=None):
+def translate(msg_id, msg_plural=None, plural_number=None, domain=None, locale_dir=None, lang=None):
     domain = domain or os.getenv(INTERNATIONALIZATION_DOMAIN, None)
-    localedir = localedir or os.getenv(LOCALE_DIR, '{}/locale'.format(os.getcwd()))
+    locale_dir = locale_dir or os.getenv(LOCALE_DIR, '{}/locale'.format(os.getcwd()))
     lang = lang or os.getenv(LANGUAGE, 'zh_CN')
     if domain is None:
         raise errors.DomainNotExist
-    if not (os.path.exists(localedir) and os.path.isdir(localedir)):
-        raise errors.LocaleDirNotExist(localedir)
+    if not (os.path.exists(locale_dir) and os.path.isdir(locale_dir)):
+        raise errors.LocaleDirNotExist(locale_dir)
     if msg_plural is not None and plural_number is not None:
-        return gettext.translation(domain, localedir, languages=[lang]).ngettext(msg_id, msg_plural, plural_number)
-    return gettext.translation(domain, localedir, languages=[lang]).gettext(msg_id)
+        return gettext.translation(domain, locale_dir, languages=[lang]).ngettext(msg_id, msg_plural, plural_number)
+    return gettext.translation(domain, locale_dir, languages=[lang]).gettext(msg_id)
 
 
 
