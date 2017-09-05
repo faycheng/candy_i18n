4b022b301147f494b42d08200ef25471a6cfa0ff
faycheng <fei.cheng@daocloud.io>
2017-09-05 14:54:45 +0800

    define LocaleDirNotExist error

--- candy_i18n/errors.py
+++ candy_i18n/errors.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 
+
 class BaseError(BaseException):
     ERROR_MSG_TEMPLATE = 'i18n domain not exist'
 
@@ -12,4 +13,9 @@ class DomainNotExist(BaseError):
         super(DomainNotExist, self).__init__(self.message)
 
 
+class LocaleDirNotExist(BaseError):
+    ERROR_MSG_TEMPLATE = 'locale dir {} not exist'
 
+    def __init__(self, locale_dir):
+        self.message = self.ERROR_MSG_TEMPLATE.format(locale_dir)
+        super(LocaleDirNotExist, self).__init__(self.message)
