c483173683bc012d6aff229bfe2f2a75433cf740
faycheng <fei.cheng@daocloud.io>
2017-09-05 18:31:44 +0800

    init cli

---
+++ candy_i18n/cli.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+import fire
+
+
+def init():
+    pass
+
+
+def extract():
+    pass
+
+
+def compile():
+    pass
+
+
+def list():
+    pass
+
+
+def translate():
+    pass
+
+
+if __name__ == '__main__':
+    fire.Fire()
