da57bcd9a61b1fda056243eaf17760926f8e3f7d
faycheng <fei.cheng@daocloud.io>
2017-09-06 17:10:33 +0800

    move: candy_i18n/cli.py -> cli.py

--- cli.py
+++ cli.py
@@ -6,7 +6,7 @@ import json
 import pickle
 import polib
 
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 
 from candy_i18n.extract import extract
 from candy_i18n import po
